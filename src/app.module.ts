d2fd651f2d92c758831b735129f73357ed8e53cc
saikiran <saikiran@viamagus.com>
2024-09-20 14:19:29 +0530

    mysql db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { TasksModule } from './tasks/tasks.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
+        type: 'mysql',
         host: configService.get('DB_HOST'),
         port: +configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
